codeaurora-unoffical/platform-packages-apps-Stk

packages/app/Stk: Fix ANR in phone app if CatService instance is unavailable
Change-Id: Ic78d698be53fb389d36856dc52e72b055d6a829f
CRs-Fixed: 283638

src/com/android/stk/StkAppService.java
@@ -175,21 +175,6 @@ public class StkAppService extends Service implements Runnable {
 
     @Override
     public void onCreate() {
-        // Initialize members
-        mStkService = com.android.internal.telephony.cat.CatService
-                .getInstance();
-
-        // NOTE mStkService is a singleton and continues to exist even if the GSMPhone is disposed
-        //   after the radio technology change from GSM to CDMA so the PHONE_TYPE_CDMA check is
-        //   needed. In case of switching back from CDMA to GSM the GSMPhone constructor updates
-        //   the instance. (TODO: test).
-        if ((mStkService == null)
-                && (TelephonyManager.getDefault().getPhoneType()
-                                != TelephonyManager.PHONE_TYPE_CDMA)) {
-            CatLog.d(this, " Unable to get Service handle");
-            return;
-        }
-
         mCmdsQ = new LinkedList<DelayedCmd>();
         Thread serviceThread = new Thread(null, this, "Stk App Service");
         serviceThread.start();
@@ -201,6 +186,20 @@ public class StkAppService extends Service implements Runnable {
 
     @Override
     public void onStart(Intent intent, int startId) {
+        // Get instance of CatService
+        mStkService = com.android.internal.telephony.cat.CatService
+                .getInstance();
+        if (mStkService == null) {
+            CatLog.d(this, " Unable to get Service handle, stopping StkAppService");
+            // Unistall StkApp, Clear Idle text, Stop StkAppService
+            // If the CatService is not running and the StkAppService is going
+            // away then there is no way StkApp can communicate to gstk
+            StkAppInstaller.unInstall(mContext);
+            mNotificationManager.cancel(STK_NOTIFICATION_ID);
+            stopSelf();
+            return;
+        }
+
         waitForLooper();
 
         // onStart() method can be passed a null intent
@@ -243,6 +242,7 @@ public class StkAppService extends Service implements Runnable {
     public void onDestroy() {
         waitForLooper();
         mServiceLooper.quit();
+        sInstance = null;
     }
 
     @Override
@@ -393,33 +393,23 @@ public class StkAppService extends Service implements Runnable {
     }
 
     private void handleIccStatusChange(Bundle args) {
-        Boolean RadioStatus = args.getBoolean("RADIO_AVAILABLE");
+        SimRefreshResponse state = new SimRefreshResponse();
+        state.refreshResult = SimRefreshResponse
+                .refreshResultFromRIL(args.getInt("REFRESH_RESULT"));
 
-        if (RadioStatus == false) {
-            CatLog.d(this, "RADIO_OFF_OR_UNAVAILABLE received");
-            // Unistall STKAPP, Clear Idle text, Stop StkAppService
-            StkAppInstaller.unInstall(mContext);
+        CatLog.d(this, "Icc Refresh Result: " + state.refreshResult);
+        if ((state.refreshResult == SimRefreshResponse.Result.SIM_INIT)
+                || (state.refreshResult == SimRefreshResponse.Result.SIM_RESET)) {
+            // Clear Idle Text
             mNotificationManager.cancel(STK_NOTIFICATION_ID);
-            stopSelf();
-        } else {
-            SimRefreshResponse state = new SimRefreshResponse();
-            state.refreshResult = SimRefreshResponse.refreshResultFromRIL(
-                    args.getInt("REFRESH_RESULT"));
+            mIdleModeTextCmd = null;
+        }
 
-            CatLog.d(this, "Icc Refresh Result: "+ state.refreshResult);
-            if ((state.refreshResult == SimRefreshResponse.Result.SIM_INIT) ||
-                (state.refreshResult == SimRefreshResponse.Result.SIM_RESET)) {
-                // Clear Idle Text
-                mNotificationManager.cancel(STK_NOTIFICATION_ID);
-                mIdleModeTextCmd = null;
-            }
-
-            if (state.refreshResult == SimRefreshResponse.Result.SIM_RESET) {
-                // Uninstall STkmenu
-                StkAppInstaller.unInstall(mContext);
-                mCurrentMenu = null;
-                mMainCmd = null;
-            }
+        if (state.refreshResult == SimRefreshResponse.Result.SIM_RESET) {
+            // Uninstall STkmenu
+            StkAppInstaller.unInstall(mContext);
+            mCurrentMenu = null;
+            mMainCmd = null;
         }
     }
 

OMFGB/packages_apps_Stk

Change-Id: I4035a515ea3a2803befe2aab043cb86faa15e5d6

src/com/android/stk/StkAppService.java
@@ -165,21 +165,6 @@ public class StkAppService extends Service implements Runnable {
 
     @Override
     public void onCreate() {
-        // Initialize members
-        mStkService = com.android.internal.telephony.cat.CatService
-                .getInstance();
-
-        // NOTE mStkService is a singleton and continues to exist even if the GSMPhone is disposed
-        //   after the radio technology change from GSM to CDMA so the PHONE_TYPE_CDMA check is
-        //   needed. In case of switching back from CDMA to GSM the GSMPhone constructor updates
-        //   the instance. (TODO: test).
-        if ((mStkService == null)
-                && (TelephonyManager.getDefault().getPhoneType()
-                                != TelephonyManager.PHONE_TYPE_CDMA)) {
-            CatLog.d(this, " Unable to get Service handle");
-            return;
-        }
-
         mCmdsQ = new LinkedList<DelayedCmd>();
         Thread serviceThread = new Thread(null, this, "Stk App Service");
         serviceThread.start();
@@ -191,6 +176,20 @@ public class StkAppService extends Service implements Runnable {
 
     @Override
     public void onStart(Intent intent, int startId) {
+        // Get instance of CatService
+        mStkService = com.android.internal.telephony.cat.CatService
+                .getInstance();
+        if (mStkService == null) {
+            CatLog.d(this, " Unable to get Service handle, stopping StkAppService");
+            // Unistall StkApp, Clear Idle text, Stop StkAppService
+            // If the CatService is not running and the StkAppService is going
+            // away then there is no way StkApp can communicate to gstk
+            StkAppInstaller.unInstall(mContext);
+            mNotificationManager.cancel(STK_NOTIFICATION_ID);
+            stopSelf();
+            return;
+        }
+
         waitForLooper();
 
         // onStart() method can be passed a null intent
@@ -231,6 +230,7 @@ public class StkAppService extends Service implements Runnable {
     public void onDestroy() {
         waitForLooper();
         mServiceLooper.quit();
+        sInstance = null;
     }
 
     @Override
@@ -365,33 +365,22 @@ public class StkAppService extends Service implements Runnable {
     }
 
     private void handleIccStatusChange(Bundle args) {
-        Boolean RadioStatus = args.getBoolean("RADIO_AVAILABLE");
+        IccRefreshResponse state = new IccRefreshResponse();
+        state.refreshResult = IccRefreshResponse.Result.values()[args.getInt("REFRESH_RESULT")];
 
-        if (RadioStatus == false) {
-            CatLog.d(this, "RADIO_OFF_OR_UNAVAILABLE received");
-            // Unistall STKAPP, Clear Idle text, Stop StkAppService
-            StkAppInstaller.unInstall(mContext);
+        CatLog.d(this, "Icc Refresh Result: " + state.refreshResult);
+        if ((state.refreshResult == IccRefreshResponse.Result.SIM_INIT)
+                || (state.refreshResult == IccRefreshResponse.Result.SIM_RESET)) {
+            // Clear Idle Text
             mNotificationManager.cancel(STK_NOTIFICATION_ID);
-            stopSelf();
-        } else {
-            IccRefreshResponse state = new IccRefreshResponse();
-            state.refreshResult = IccRefreshResponse.Result
-                    .values()[args.getInt("REFRESH_RESULT")];
+            mIdleModeTextCmd = null;
+        }
 
-            CatLog.d(this, "Icc Refresh Result: "+ state.refreshResult);
-            if ((state.refreshResult == IccRefreshResponse.Result.SIM_INIT) ||
-                (state.refreshResult == IccRefreshResponse.Result.SIM_RESET)) {
-                // Clear Idle Text
-                mNotificationManager.cancel(STK_NOTIFICATION_ID);
-                mIdleModeTextCmd = null;
-            }
-
-            if (state.refreshResult == IccRefreshResponse.Result.SIM_RESET) {
-                // Uninstall STkmenu
-                StkAppInstaller.unInstall(mContext);
-                mCurrentMenu = null;
-                mMainCmd = null;
-            }
+        if (state.refreshResult == IccRefreshResponse.Result.SIM_RESET) {
+            // Uninstall STkmenu
+            StkAppInstaller.unInstall(mContext);
+            mCurrentMenu = null;
+            mMainCmd = null;
         }
     }
 